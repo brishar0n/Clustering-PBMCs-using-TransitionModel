a6165c9af2929705e9b92ba2a87545b911586d90
Brigitte <114067290+brishar0n@users.noreply.github.com>
2025-01-07 09:42:17 +0700

    Delete preprocess.py

--- preprocess.py
+++
@@ -1,9 +0,0 @@
-import scanpy as sc
-
-def load_data(data_path):
-    """
-    Loads the PBMC dataset from 10X Genomics.
-    """
-    adata = sc.read_10x_mtx(data_path, var_names='gene_symbols', cache=True)
-    adata.var_names_make_unique()
-    return adata
